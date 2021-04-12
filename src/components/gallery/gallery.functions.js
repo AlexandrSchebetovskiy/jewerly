07d8545e2dd1ad5b596e320cb4f5c1a566cab8ae
Alexandr <s.shebet@gmail.com>
2021-04-12 15:40:47 +0300

    gallery modal

--- src/components/gallery/gallery.functions.js
+++ src/components/gallery/gallery.functions.js
@@ -1,11 +1,13 @@
-import {Modal} from '@/components/modal/Modal';
+import {Modal} from '@/components/modal/Modal'
+import {GalleryModal} from '@/components/modal/GalleryModal'
+
 
 import {$} from '@core/dom'
 export function createGalleryModal(id, data) {
   const $root = $('.gallery')
 
   const $el = $.create(Modal.tagName, Modal.className)
-  const modal = new Modal($el, id, data)
+  const modal = new GalleryModal($el, id, data)
   console.log($root);
   $el.html(modal.toHTML())
   $root.append($el)
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -3,34 +3,12 @@ import {$} from '@core/dom'
 export class Modal extends AppComponent {
   static className ='modal'
   static tagName = 'div'
-  constructor($root, id, data) {
+  constructor($root) {
     super($root, {
       name: 'Modal',
       listeners: ['click']
     })
-    this.data = data
-    this.id = id
     this.onClick = this.onClick.bind(this)
-  }
-  toHTML() {
-    let current
-    this.data.forEach(item => {
-      if (item.id == this.id) {
-        current = item
-      }
-    })
-    return `
-    <div class="modal__wrap">
-      <div class="modal__title">${current.name}</div>
-      <div class="modal__img">
-        <img class="modal__image" src=${current.img} alt="">
-      </div>
-      <div class="modal__close">
-        <span id="first"></span>
-        <span id="second"></span>
-      </div>
-    </div>
-    `
   }
   onClick(event) {
     const el= $(event.target)
