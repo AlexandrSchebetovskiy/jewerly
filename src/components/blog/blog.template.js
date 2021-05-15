bfa36ce1cbfd93213a07bb30820303858b643a28
Alexandr <s.shebet@gmail.com>
2021-05-15 20:34:24 +0300

    article styled

--- src/components/blog/blog.template.js
+++ src/components/blog/blog.template.js
@@ -9,7 +9,9 @@ export function getBlogTemplate(posts) {
           <a class="article__title">
             <h3>${post.title}</h3>
           </a>
-          <div class="article__date">${post.date}</div>
+          <div class="article__date">
+            ${post.date.month} ${post.date.day}, ${post.date.year} 
+          </div>
           <div class="article__text">${sliceText(post.text)}</div>
           <a href="#article/${post.id}">
             <button class="article__button">read more</button>
